3be65db7bf91199ffdc3c998962ec8346d1605fd
shreyas bole <shreyasbole4@gmail.com>
2023-07-25 20:15:47 +0530

    while / do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+// while (condition) {
+    
+// }
+
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// do while loop
+
+// let score = 11;  // if score is 11, it will still run once
+
+// do {
+//     console.log(`Your score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
