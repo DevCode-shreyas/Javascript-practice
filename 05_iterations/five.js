ca4a73ac2980381032d5b35bfb56d3ef68a6e32c
shreyas bole <shreyasbole4@gmail.com>
2023-07-26 18:23:42 +0530

    forEach loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,22 @@
+const coding = ["js", "python", "ruby", "java", "c++", "c#"];
+
+// coding.forEach(function(item){
+//     console.log(item);
+// })
+
+
+
+// coding.forEach((item) => {
+//     console.log(item);
+// });
+
+// function print(item){
+//     console.log(item);
+// }
+
+// coding.forEach(print);
+
+
+// coding.forEach((item, index,arr) => {
+//     console.log(item, index, arr);
+// });
