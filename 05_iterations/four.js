c2ddc2bbc6d9d7dd9b800ab53d6f9eda4eedcec5
shreyas bole <shreyasbole4@gmail.com>
2023-07-26 15:32:24 +0530

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+// const myObj= {
+//     js : "Javascript",
+//     py :"Python",
+//     rb : "Ruby"
+// }
+
+// for (const key in myObj) {
+//     console.log( key,myObj[key]);
+// }
+
+
+const programming = ["Javascript", "Python", "Ruby"];
+
+for (const key in programming) {
+    console.log(key , programming[key]);
+}
