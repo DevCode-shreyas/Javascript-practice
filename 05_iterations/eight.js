c53f35c30c99b34639de623c6cb918beecb1d3d1
shreyas bole <shreyasbole4@gmail.com>
2023-07-27 18:27:15 +0530

    normal

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,36 +1,54 @@
-// reduce
+// // reduce
 
-// const muNums = [1, 2, 3];
+// // const muNums = [1, 2, 3];
 
-// const sum = muNums.reduce((acc, num) => {
-//     // console.log(acc, num);
-//   return acc + num;
+// // const sum = muNums.reduce((acc, num) => {
+// //     // console.log(acc, num);
+// //   return acc + num;
+// // }, 0);
+
+// // console.log(sum);
+
+// const shoppingCart = [
+//     {
+//         itemName: "js course",
+//         price: 199
+//     },
+//     {
+//         itemName: "py course",
+//         price: 999
+//     },
+//     {
+//         itemName: "Rust course",
+//         price: 500
+//     },
+//     {
+//         itemName: "php course",
+//         price: 399
+//     },
+// ]
+
+// const priceToPay=shoppingCart.reduce((acc,item)=>{
+//     return acc + item.price
 // }, 0);
 
-// console.log(sum);
+// console.log(priceToPay);
+
+// function userDetails(username) {
+//     if (username) {
+//       console.log(salary);
+//       console.log(age);
+//       let age = 30;
+//       var salary = 10000;
+//     }
+//     console.log(salary);
+//     console.log(age);
+//   }
+//   userDetails("John")
 
 
-const shoppingCart = [
-    {
-        itemName: "js course",
-        price: 199
-    },
-    {
-        itemName: "py course",
-        price: 999
-    },
-    {
-        itemName: "Rust course",
-        price: 500
-    },
-    {
-        itemName: "php course",
-        price: 399
-    },
-]
 
-const priceToPay=shoppingCart.reduce((acc,item)=>{
-    return acc + item.price 
-}, 0);
 
-console.log(priceToPay);
+
+
+
